71d7658ddaef9fa2dbf59635082233626732f94b
null <register_for_bonic@163.com>
2021-01-19 20:25:47 +0800

    Update CustomRulesDialog.java

--- core/src/mindustry/ui/dialogs/CustomRulesDialog.java
+++ core/src/mindustry/ui/dialogs/CustomRulesDialog.java
@@ -140,7 +140,7 @@ public class CustomRulesDialog extends BaseDialog{
         title("@rules.title.resourcesbuilding");
         check("@rules.infiniteresources", b -> rules.infiniteResources = b, () -> rules.infiniteResources);
         check("@rules.reactorexplosions", b -> rules.reactorExplosions = b, () -> rules.reactorExplosions);
-        check("@rules.schematic", b -> rules.schematicsAllowed = b, () -> rules.schematicsAllowed);
+        //check("@rules.schematic", b -> rules.schematicsAllowed = b, () -> rules.schematicsAllowed);
         check("@rules.coreincinerates", b -> rules.coreIncinerates = b, () -> rules.coreIncinerates);
         number("@rules.buildcostmultiplier", false, f -> rules.buildCostMultiplier = f, () -> rules.buildCostMultiplier, () -> !rules.infiniteResources);
         number("@rules.buildspeedmultiplier", f -> rules.buildSpeedMultiplier = f, () -> rules.buildSpeedMultiplier, 0.001f, 50f);
